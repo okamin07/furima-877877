8c3b2433808fcfaed14167a70ed74eb5ca35c956
okamin07 <ryoya.okamura@di-v.co.jp>
2021-01-26 17:56:32 +0900

    Activehash_アソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,14 @@ class Item < ApplicationRecord
   has_many:items 
   has_many:orders 
   has_one_attached :image
+
+  validates :image,:name,:details,:price,:category_id,:status_id,:delivery_fee_id,:shiping_address_id,:shiping_date_id,:price,:user_id, presence: true 
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shiping_date
+  belongs_to :delivery_fee
+  belongs_to :shiping_address
 end
+
