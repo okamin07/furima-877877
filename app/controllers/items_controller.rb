e04abc1c9e89713341c1dbbd953dc9b36d0580a0
okamin07 <ryoya.okamura@di-v.co.jp>
2021-02-18 20:27:33 +0900

    商品情報編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index ,:show]
+  before_action :move_to_index, only: [:edit]
+
 
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -22,8 +24,29 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to controller: :items, action: :index
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :image, :details, :price ,:shiping_address_id,:category_id,:status_id,:delivery_fee_id,:shiping_address_id,:shiping_date_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    user = item.user
+    unless user == current_user
+      redirect_to action: :index
+    end
   end
 end
