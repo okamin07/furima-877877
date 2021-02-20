8e87341fd3ca182aa1df2b6b411817fb8bc6b5ce
okamin07 <ryoya.okamura@di-v.co.jp>
2021-02-20 21:24:13 +0900

    set_item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index ,:show]
   before_action :move_to_index, only: [:edit]
-
+  before_action :set_item, only:[:edit,:show,:update]
 
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -21,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show 
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to controller: :items, action: :index
     else
@@ -40,6 +37,10 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :image, :details, :price ,:shiping_address_id,:category_id,:status_id,:delivery_fee_id,:shiping_address_id,:shiping_date_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def move_to_index
